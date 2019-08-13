3316133c6e5ed2bea43c4b00832a9933ec28a018
Absolom Mugwagwa <absolom@Werner-Terblanche.local>
2019-08-13 12:08:38 +0200

    adding invite html

--- resources/views/email/invite.blade.php
+++ resources/views/email/invite.blade.php
@@ -1,11 +1,226 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>You're invited</title> 
-    </head>
-    <body>
-        
-        <h1>Welcome to Urban Burn!</h1>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"
+xmlns:v="urn:schemas-microsoft-com:vml"
+xmlns:o="urn:schemas-microsoft-com:office:office">
+<head>
+<!--[if gte mso 9]><xml>
+<o:OfficeDocumentSettings>
+<o:AllowPNG/>
+<o:PixelsPerInch>96</o:PixelsPerInch>
+</o:OfficeDocumentSettings>
+</xml><![endif]-->
+<title>99c Presents - FORESHORE FIRE</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0 " />
+<meta name="format-detection" content="telephone=no"/>
+<style type="text/css">
+body {
+	margin: 0;
+	padding: 0;
+	-webkit-text-size-adjust: 100% !important;
+	-ms-text-size-adjust: 100% !important;
+	-webkit-font-smoothing: antialiased !important;
+}
+img {
+	border: 0 !important;
+	outline: none !important;
+}
+p {
+	Margin: 0px !important;
+	Padding: 0px !important;
+}
+table {
+	border-collapse: collapse;
+	mso-table-lspace: 0px;
+	mso-table-rspace: 0px;
+}
+td, a, span {
+	border-collapse: collapse;
+	mso-line-height-rule: exactly;
+}
+.ExternalClass * {
+	line-height: 100%;
+}
+.em_defaultlink a {
+	color: inherit !important;
+	text-decoration: none !important;
+}
+ @media only screen and (min-width:481px) and (max-width:599px) {
+.em_main_table {
+	width: 100% !important;
+}
+.em_wrapper {
+	width: 100% !important;
+}
+.em_hide {
+	display: none !important;
+}
+.em_full_img {
+	width: 100% !important;
+	height: auto !important;
+	max-width: none !important;
+}
+.em_full_img img {
+	width: 100% !important;
+	height: auto !important;
+	max-width: none !important;
+}
+.em_center {
+	text-align: center !important;
+}
+.em_side10 {
+	width: 10px !important;
+}
+.em_ptop {
+	padding-top: 20px !important;
+}
+.em_ptop10 {
+	padding-top: 10px !important;
+}
+.em_pbottom {
+	padding-bottom: 20px !important;
+}
+.em_p20 {
+	padding: 20px 0 !important;
+}
+.em_h20 {
+	height: 20px !important;
+	font-size: 1px!important;
+	line-height: 1px!important;
+}
+.em_clear {
+	clear: both !important;
+	width: 100% !important;
+	display: block !important;
+}
+.em_border {
+	border-bottom: 1px solid #d7d7d7 !important;
+}
+.em_hauto {
+	height: auto !important;
+}
+.em_p10 {
+	padding: 10px 0 !important;
+}
+.em_pbottom10 {
+	padding-bottom: 10px !important;
+}
+}
+@media screen and (max-width: 480px) {
+.em_main_table {
+	width: 100% !important;
+}
+.em_wrapper {
+	width: 100% !important;
+}
+.em_hide {
+	display: none !important;
+}
+.em_full_img {
+	width: 100% !important;
+	height: auto !important;
+	max-width: none !important;
+}
+.em_full_img img {
+	width: 100% !important;
+	height: auto !important;
+	max-width: none !important;
+}
+.em_center {
+	text-align: center !important;
+}
+.em_side10 {
+	width: 10px !important;
+}
+.em_ptop {
+	padding-top: 20px !important;
+}
+.em_ptop10 {
+	padding-top: 10px !important;
+}
+.em_pbottom {
+	padding-bottom: 20px !important;
+}
+.em_pbottom10 {
+	padding-bottom: 10px !important;
+}
+.em_p10 {
+	padding: 10px 0 !important;
+}
+.em_p20 {
+	padding: 20px 0 !important;
+}
+.em_h20 {
+	height: 20px !important;
+	font-size: 1px!important;
+	line-height: 1px!important;
+}
+.em_clear {
+	clear: both !important;
+	width: 100% !important;
+	display: block !important;
+}
+u + .em_body .em_full_wrap {
+	width: 100% !important;
+	width: 100vw !important;
+}
+.em_font12 {
+	font-size: 12px!important;
+	line-height: 30px!important;
+	height: 30px !important;
+}
+.em_border {
+	border-bottom: 1px solid #d7d7d7 !important;
+}
+.em_hauto {
+	height: auto !important;
+}
+}
+</style>
+</head>
+<body class="em_body" style="margin:0px auto; padding:0px;">
+<table width="100%" border="0" cellspacing="0" cellpadding="0" class="em_full_wrap">
+  <tr>
+    <td align="center" valign="top"><table align="center" width="600" border="0" cellspacing="0" cellpadding="0" class="em_main_table" style="width:600px; table-layout:fixed;">
+        <tr>
+          <td valign="top" align="center">
+			  
+	
 
-    </body>
+			<!--C1 Section Start-->
+
+            <table width="600" border="0" cellspacing="0" cellpadding="0" align="center" style="width:600px;" class="em_wrapper">
+              <tr>
+                <td valign="top" align="center" style="padding:0px 20px 20px;">
+					<table width="560" border="0" cellspacing="0" cellpadding="0" align="center" style="width:560px;" class="em_wrapper">
+                    <tr>
+                      <td valign="top" align="center"><img src="http://99dev.co.za/99party/99cParty_1.jpg" width="560" height="auto" alt="99C PRESENTS FORESHORE FIRE - 6 SEPTEMBER" border="0" style="display:block; max-width:560px; font-family:Gotham, 'Helvetica Neue', Helvetica, Arial, 'sans-serif'; font-size:15px; line-height:20px; color:#000000;" class="em_full_img" /></td>
+                    </tr>
+					<tr>
+                      <td class="em_defaultlink" valign="top" align="center" style="font-family:Gotham, 'Helvetica Neue', Helvetica, Arial, 'sans-serif'; color:#011bbc; font-size:24px; background-color: #fed2c7; padding-top: 15px; padding-bottom: 10px; font-weight: 800; line-height:24px;">&nbsp;FRIDAY 6 SEPTEMBER</td>
+                    </tr>
+					<tr>
+                      <td class="em_defaultlink" valign="top" align="center" style="font-family:Gotham, 'Helvetica Neue', Helvetica, Arial, 'sans-serif'; color:#011bbc; font-size:13px; background-color: #fed2c7; padding: 5px 15px 5px 15px; line-height:24px;"> Like a moth to a flame burned by the fire, this party’s not to be missed &#45; just you see what transpires! We&#39;re letting go of old energy and logos, and celebrating new vibes. Look like a flame and dance away in your festival best</td>
+                    </tr>
+                    <tr>
+                      <td valign="top" align="center" style="padding: 10px 0 10px 0; background-color: #fed2c7;">
+						  <table width="220" border="0" cellspacing="0" cellpadding="0" align="center" style="width:220px; border:3px solid #011bbc; background-color: #fed2c7">
+                          <tr style="background-color: #fed2c7">
+                            <td class="em_defaultlink" valign="middle" align="center" bgcolor="#fed2c7" height="45" style="font-family:Gotham, 'Helvetica Neue', Helvetica, Arial, 'sans-serif'; font-size:18px; color:#011bbc;"><a href="#" target="_blank" style="text-decoration:none; font-weight: 500; color:#011bbc; background-color: #fed2c7; line-height:45px; display:block;">CLICK HERE TO RSVP</a></td>
+                          </tr>
+                        </table></td>
+                    </tr>
+					<tr>
+                      <td valign="top" align="center"><img src="http://99dev.co.za/99party/99cParty_2.jpg" width="560" height="auto" alt="99C PRESENTS FORESHORE FIRE - 6 SEPTEMBER" border="0" style="display:block; max-width:560px; font-family:Gotham, 'Helvetica Neue', Helvetica, Arial, 'sans-serif'; font-size:15px; line-height:20px; color:#000000;" class="em_full_img" /></td>
+                    </tr>
+                  </table></td>
+              </tr>
+            </table>
+</td>
+        </tr>
+      </table></td>
+  </tr>
+</table>
+</body>
 </html>
